a1b482f8809d4d9e07cfa2b060c8fd9bdfa42233
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-11-22 14:49:50 -0800

    before debug (only Homepage works)

--- index.js
+++ index.js
@@ -3,10 +3,74 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("");
+  //   if (req.url === "/") {
+  //     fs.readFile(
+  //       path.join(__dirname, "public", "index.html"),
+  //       (err, content) => {
+  //         if (err) throw err;
+  //         res.writeHead(200, { "Content-Type": "text/html" });
+  //         res.end(content);
+  //       }
+  //     );
+  //   }
+
+  //   if (req.url === "/about") {
+  //     fs.readFile(
+  //       path.join(__dirname, "public", "about.html"),
+  //       (err, content) => {
+  //         if (err) throw err;
+  //         res.writeHead(200, { "Content-Type": "text/html" });
+  //         res.end(content);
+  //       }
+  //     );
+  //   }
+
+  // build file path
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  // extension of file
+  let extname = path.extname(filePath);
+
+  // intial content type
+  let contentType = "text/html";
+
+  // check ext and set content type
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
   }
+
+  // read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENONET") {
+        // page not found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        // some server error
+        res.writeHead(500);
+        res.end(`Server Errorrr: ${err.code}`);
+      }
+    } else {
+      // success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5001;
