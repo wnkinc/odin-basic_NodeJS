1f325a42bb3c5253033219c20cdca484c139f510
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-11-29 19:58:33 -0800

    Add routes and routers

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/bookRouter");
+// const indexRouter = require("./routes/indexRouter");
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+// app.use("/", indexRouter);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 app.get("/contact-me", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "contact-me.html"));
